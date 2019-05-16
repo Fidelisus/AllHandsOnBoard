6455e7811875ebc0f24adc4cc4c27f6b2024c9cd
JanSchilling <46085342+JanSchilling@users.noreply.github.com>
2019-05-16 21:58:09 +0200

    Add files via upload

---
+++ AllHandsOnBoard/AllHandsOnBoardFrontend/src/app/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+  user_id: number;
+  name: string;
+  surname: string;
+  email: string;
+  occupation: string;
+  index_no: number;
+  academic_title: string;
+  department: string;
+  points: number;
+}
