70f49b9473e5f1f56070d91a68b9fc6ac988f4b5
JanSchilling <46085342+JanSchilling@users.noreply.github.com>
2019-05-16 22:06:32 +0200

    Update app-routing.module.ts

--- AllHandsOnBoard/AllHandsOnBoardFrontend/src/app/app-routing.module.ts
+++ AllHandsOnBoard/AllHandsOnBoardFrontend/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {TaskListComponent} from './task-list/task-list.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'task-list', component: TaskListComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
 
