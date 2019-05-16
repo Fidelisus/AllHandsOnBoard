ecd06540755173b61f3e0dafc972bc26fbefa31a
JanSchilling <46085342+JanSchilling@users.noreply.github.com>
2019-05-16 22:05:02 +0200

    Update app.module.ts

--- AllHandsOnBoard/AllHandsOnBoardFrontend/src/app/app.module.ts
+++ AllHandsOnBoard/AllHandsOnBoardFrontend/src/app/app.module.ts
@@ -7,17 +7,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RestService } from './rest.service';
 import { TaskListComponent } from './task-list/task-list.component';
-import {RouterModule, Routes} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
-
-const appRoutes: Routes = [
-  { path: 'task-list', component: TaskListComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
-];
 
 @NgModule({
   declarations: [
